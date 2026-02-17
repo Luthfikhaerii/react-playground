dc46907a67d80bad809af3876433ab0084cab8e5
luthfikhaerii <luthfikhaeri00@gmail.com>
2026-02-17 23:20:58 +0700

    react playground

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-yellow-600">Home Page</h1>
+      <p>Welcome to the home page of our React application.</p>
+    </div>
+  )
+}
